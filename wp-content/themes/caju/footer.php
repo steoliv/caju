70fd1ba43b7dd71a850b97a8720cb5a74a25ca00
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-11-30 02:34:19 -0300

    CRiando estrutura do footer e fazendo chamada do menu

--- wp-content/themes/caju/footer.php
+++ wp-content/themes/caju/footer.php
@@ -1,3 +1,16 @@
-carregando footer
+
+
+<footer class="site-footer">
+    <div class="container">
+
+        <div class="copyright">
+            Copyright - All Rights Reserved
+        </div>
+        <nav class="footer-menu">
+            <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'depth' => 1)); ?>
+        </nav>
+    </div>
+</footer>
+
 
 <?php wp_footer(); ?>
