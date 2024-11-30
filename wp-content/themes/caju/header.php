2a34f44e2be6c7d89e74e1e38927ce8514dbcaa9
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-11-30 02:07:28 -0300

    removendo chamada do css e do bootstrap do header porque passarão a serem carregados pelo wp_head com a função que criamos em functions.php

--- wp-content/themes/caju/header.php
+++ wp-content/themes/caju/header.php
@@ -6,9 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="description" content="<?php bloginfo('description'); ?>">
   <title><?php bloginfo('name'); ?> <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> 
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-
+  <?php wp_head(); ?>
 
 </head>
 
