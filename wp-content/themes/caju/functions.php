5451d450e574c031807b8713492b5468e11a9f4a
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-12-02 01:36:14 -0300

    Incluindo sidebar no tema

--- wp-content/themes/caju/functions.php
+++ wp-content/themes/caju/functions.php
@@ -33,3 +33,18 @@
   };
   add_action( 'after_setup_theme', 'caju_config', 0);
   
+
+  add_action('widgets_init', 'caju_sidebars' );
+  function caju_sidebars(){
+    register_sidebar(
+      array(
+        'name' => ' Blog Sidebar',
+        'id' => 'sidebar-blog',
+        'description' => ' This is the Blog Sidebar. You can add your widgets here.',
+        'before_widget' => '<div class="widget-wrapper">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4 class="widget-title>',
+        'after_title' => '</h4>'
+        )
+    );
+  }
