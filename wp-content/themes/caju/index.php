6ddffbd4cc779ff9b288cdd4ed2cac792fc1b4d4
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-12-02 03:03:04 -0300

    Refatorando estrutura da index

--- wp-content/themes/caju/index.php
+++ wp-content/themes/caju/index.php
@@ -14,39 +14,44 @@ get_header();
     <section class="blog-home">
         <div class="container">
             <div class="row">
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <article class="article-loop col-md-3">
-                    <header>
-                        <h2>
-                            <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                                <?php the_title(); ?>
-                            </a>
-                        </h2>
-                        <div class="article-img">
-                            <?php the_post_thumbnail( 'medium'); ?>
-                        </div>
-                        <div class="article-info">
-                            <p> Postado em <?php echo get_the_date(); ?>  por <?php echo get_the_author_posts_link(); ?></p>
-                            <p> Categories: <?php the_category( '' ); ?></p>
-                            <p> Tags: <?php the_tags( '', ', '); ?></p>
-                        </div>
-                    </header>
-                    <div class="article-content">
-                        <?php the_excerpt(); ?>
+                <div class="col-md-8 col-12 blog-list">
+                    <div class="container">
+                        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                            <article class="article-loop col-12">
+                                <header>
+                                    <h2>
+                                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                                            <?php the_title(); ?>
+                                        </a>
+                                    </h2>
+                                    <div class="article-img">
+                                        <a href="the_permalink();">
+                                            <?php the_post_thumbnail( 'medium'); ?>
+                                        </a>
+                                    </div>
+                                    <div class="article-info">
+                                        <p> Postado em <?php echo get_the_date(); ?>  por <?php echo get_the_author_posts_link(); ?></p>
+                                        <p> Categories: <?php the_category( '' ); ?></p>
+                                        <p> Tags: <?php the_tags( '', ', '); ?></p>
+                                    </div>
+                                </header>
+                                <div class="article-content">
+                                    <?php the_excerpt(); ?>
+                                </div>
+                            </article>
+                        <?php endwhile; else : ?>
+                                <article>
+                                    <p>Sorry, no posts were found!</p>
+                                </article>
+                        <?php endif; ?>
                     </div>
-                </article>
-                <?php endwhile; else : ?>
-                    <article>
-                        <p>Sorry, no posts were found!</p>
-                    </article>
-                    <?php endif; ?>
-                    
+                </div>
+
+                <div class="sidebar-blog col-md-4 col-12">
+                    <?php echo get_sidebar(); ?>
+                </div>
             </div>
         </div>
-    </section>
-
-    <section class="sidebar-blog">
-        <?php echo get_sidebar(); ?>
     </section>
 </div>
         
