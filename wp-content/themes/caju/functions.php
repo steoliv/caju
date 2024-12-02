c309ea118bfd2d15b4f9c8b9febc028038b287b9
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-12-02 00:54:03 -0300

    inserindo opção de imagem destacada nos posts

--- wp-content/themes/caju/functions.php
+++ wp-content/themes/caju/functions.php
@@ -23,6 +23,7 @@
       'width' => 1920
     );
     add_theme_support( 'custom-header', $args);
+    add_theme_support( 'post-thumbnails');
   };
   add_action( 'after_setup_theme', 'caju_config', 0);
   
