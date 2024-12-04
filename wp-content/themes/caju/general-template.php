dbd3006d1e043cad696f859ade5729fc075fca34
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-12-04 01:44:52 -0300

    Alterando o general template para deixar ele apenas carregando o conteudo da página

--- wp-content/themes/caju/general-template.php
+++ wp-content/themes/caju/general-template.php
@@ -16,25 +16,13 @@ get_header();
     <section class="general-template">
         <div class="container">
             <div class="row">
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
                     <article class="general-template-loop col-md-12">
-                        <header>
-                            <h1>
-                                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                                    <?php the_title(); ?>
-                                </a>
-                            </h1>
-                            </div>
-                        </header>
-                        <div class="general-template-content">
-                            <?php the_excerpt(); ?>
-                        </div>
+                        <?php the_content();?>
                     </article>
-                <?php endwhile; else : ?>
-                    <article>
-                        <p>Sorry, no posts were found!</p>
-                    </article>
-            <?php endif; ?>
+                <?php endwhile; ?>
+                   
+
                     
             </div>
         </div>
