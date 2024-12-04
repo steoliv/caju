3f39a63c9af798ca6ef567462d0cc9d07b0773e5
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-12-04 01:44:04 -0300

    Incluindo mais support ao tema, dessa vez para alinhar o conteúdo 100% a largura da page, deixar os embeds mais alinhados tbm, styles

--- wp-content/themes/caju/functions.php
+++ wp-content/themes/caju/functions.php
@@ -37,6 +37,11 @@
     ));
     add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style','script'));
     add_theme_support( 'title-tag');
+
+
+    add_theme_support( 'align-wide');
+    add_theme_support( 'responsive-embeds');
+    add_theme_support( 'wp-block_styles');
  
   };
   add_action( 'after_setup_theme', 'caju_config', 0);
