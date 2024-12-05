966038c91a72aa9067e30bfc63b2286795a1b177
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-12-05 09:42:32 -0300

    Implementando a chamada para posts do blog dinamico

--- wp-content/themes/caju/index.php
+++ wp-content/themes/caju/index.php
@@ -17,11 +17,27 @@ get_header();
                 <div class="col-md-8 col-12 blog-list">
                     <div class="container">
                     <div class="blog-items">
-                        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+                        <?php 
+                        
+                        $per_page = get_theme_mod( 'set_per_page', 3 );
+                        $category_include = get_theme_mod( 'set_category_include' );
+                        $category_exclude = get_theme_mod( 'set_category_exclude' );
+    
+                        $args = array(
+                            'post_type' => 'post',
+                            'posts_per_page' => $per_page,
+                            'category__in' => explode( ",", $category_include),
+                            'category__not_in' => explode( ",", $category_exclude)
+                        );
+                        
+                        $postlist = new WP_Query( $args );
+
+                        if (  $postlist->have_posts() ) : while (  $postlist->have_posts() ) :  $postlist->the_post(); 
 
                             get_template_part('parts/content');
 
-                        endwhile; ?>
+                        endwhile; 
+                        wp_reset_postdata();?>
                         
                         <div class="paginacao">
                             <div class="pages new">
