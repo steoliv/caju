3115df646c7391598178299cc89c30dc2042b0c2
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-12-05 10:42:07 -0300

    finalizando single

--- wp-content/themes/caju/parts/content-single.php
+++ wp-content/themes/caju/parts/content-single.php
@@ -1,15 +1,26 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header>
         <h1><?php the_title(); ?></h1>
+
+        <?php if( has_post_thumbnail() ): ?> 
+            <?php the_post_thumbnail( 'large'); ?>
+        <?php endif; ?>
+
         <div class="meta-info">
-            <p> Postado em <?php echo get_the_date(); ?>  por <?php echo get_the_author_posts_link(); ?></p>
-            <p> Categories: <?php the_category( '' ); ?></p>
-            <p> Tags: <?php the_tags( '', ', '); ?></p>
+            <p> 
+                Postado em <?php echo get_the_date(); ?>  por <?php echo get_the_author_posts_link(); ?><br>
+                <?php if( has_category()): ?>
+                    Categories: <?php the_category( '' ); ?><br>
+                <?php endif; ?>
+                <?php if( has_tag()): ?>
+                    Tags: <?php the_tags( '', ', '); ?>
+                <?php endif; ?>
+                </p>
         </div>
     </header>
 
-    <div class="content">
+    <div class="content-single">
         <?php the_content(); ?>
 
     </div>
-</article>
+</div>
--- wp-content/themes/caju/single.php
+++ wp-content/themes/caju/single.php
@@ -3,37 +3,41 @@
 <div id="primary">
 
     <div class="main">
-            <div class="container-fluid">
-                    <div class="container"> 
+        <div class="container-fluid">
+            <div class="container"> 
 
-                    <?php 
-                        while (have_posts()) : the_post(); 
+                <?php 
+                    while (have_posts()) : the_post(); 
 
-                        get_template_part('parts/content', 'single');
+                    get_template_part('parts/content', 'single');
 
-                    ?>
-                    
-
-                        <div class="paginacao-single">
-                            <div class="pages previous">
-                                <?php previous_post_link(); ?>
-                            </div>
-                            <div class="pages next">
-                                <?php next_post_link(); ?>
-                            </div>
-
+                ?>
+                    <div class="paginacao-single">
+                        <div class="pages previous">
+                            <?php previous_post_link(); ?>
                         </div>
-                    <?php endwhile; ?>
+                        <div class="pages next">
+                            <?php next_post_link(); ?>
+                        </div>
+
+                    </div>
+                <?php endwhile; ?>
 
 
-                    <div class="comments">
-
+                <div class="comments">
                     <?php if(comments_open()){
                         comments_template();
                     }
                     ?>
-                    </div>
+                </div>
             </div>
         </div>
-
     </div>
+
+</div>
+
+<?php
+
+get_footer();
+
+?>
