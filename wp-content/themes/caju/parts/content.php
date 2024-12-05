ae8a973cb73ea18a78cf979913a9931c3a964e21
Stefany Oliveira <desenvolvedoraweb@gmail.com>
2024-12-05 01:44:05 -0300

    finalizando index-coontent

--- wp-content/themes/caju/parts/content.php
+++ wp-content/themes/caju/parts/content.php
@@ -1,29 +1,23 @@
-<article class="article-loop col-12">
-    <header>
-        <h2>
+<article>
+    
+        <h3>
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
                 <?php the_title(); ?>
             </a>
-        </h2>
-        <?php if( has_post_thumbnail() ): ?> 
-            <div class="article-img">
-                <a href="<?php the_permalink(); ?>">
-                    <?php the_post_thumbnail( 'medium'); ?>
-                </a>
-            </div>
-        <?php endif; ?>
+        </h3>
 
-        <div class="article-info">
-            <p> Postado em <?php echo get_the_date(); ?>  por <?php echo get_the_author_posts_link(); ?></p>
-            <?php if( has_category()): ?>
-                <p> Categories: <?php the_category( '' ); ?></p>
+        <?php if( has_post_thumbnail() ): ?> 
+            <a href="<?php the_permalink(); ?>">
+                <?php the_post_thumbnail( 'medium'); ?>
+            </a>
             <?php endif; ?>
-            <?php if(has_tag()): ?>
-                <p> Tags: <?php the_tags( '', ', '); ?></p>
-            <?php endif; ?>
+            
+        <p> Postado em <?php echo get_the_date(); ?></p>
+        <?php if( has_category()): ?>
+            <p>Categorias:<?php the_category( '' ); ?></p>
+        <?php endif; ?>
+            
+        <div class="meta-info">
+            <?php echo the_excerpt(); ?>
         </div>
-    </header>
-    <div class="article-content">
-        <?php the_excerpt(); ?>
-    </div>
 </article>
